5ba02a91f948e0436b3c1d06c3f587c71ec4d0c8
Khaki01 <kh.mukashev@gmail.com>
2021-02-10 00:37:59 +0600

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function toReadable (number) {
-  
+ let n = number;
+ let arr = ['', 'one', 'two', 'three', 'four', 'five', 'six', 
+ 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen',
+ 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+
+ let largerArr = {2:'twenty', 3:'thirty', 4:'forty', 5:'fifty', 6:'sixty', 
+ 7:'seventy', 8:'eighty', 9:'ninety'};
+
+ let res = '';
+ 
+ let n3 = n%10;
+ let n2 = Math.floor((n-n3)%100/10);
+ let n1 = Math.floor((n-n2-n3)%1000/100);
+
+ if(n1!=0) res+=arr[n1] + ' hundred';
+
+ if(n2>1) {
+     if(res.length>0) res+=' ';
+     res+=largerArr[n2];
+     if(n3!=0) res+= ' ';
+     res+=arr[n3];
+    }
+ else {
+    if(res.length>0 && n2*10+n3!=0) res+=' ';
+     res+=arr[n2*10+n3]
+    };
+ if(!res) return 'zero';
+
+ return res; 
 }
